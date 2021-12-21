0252d77187e463383aad7627a329b3c5966b7ef2
DaffaKresna <1910631170068@student.unsika.ac.id>
2021-12-21 19:16:42 +0700

    Edit login page structure

--- lib/ui/login_page.dart
+++ lib/ui/login_page.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:capstone_project/ui/ui_login/Header.dart';
-import 'package:capstone_project/ui/ui_login/InputWrapper.dart';
+import 'package:capstone_project/ui/page_main.dart';
+
 
 class LoginPage extends StatelessWidget {
   @override
-  Widget build(BuildContext Context) {
+  Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: Container(
@@ -16,7 +16,22 @@ class LoginPage extends StatelessWidget {
         child: Column(
           children: <Widget>[
             SizedBox(height: 80,),
-            Header(),
+            //Header
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Center(
+                    child: Text("Login", style: TextStyle(color: Colors.white, fontSize: 40),),
+                  ),
+                  SizedBox(height: 10,),
+                  Center(
+                    child: Text("Welcome to Share N Read", style: TextStyle(color: Colors.white, fontSize: 18),),
+                  )
+                ],
+              ),
+            ),
             Expanded(
               child: Container(
                 decoration: BoxDecoration(
@@ -26,7 +41,70 @@ class LoginPage extends StatelessWidget {
                         topRight: Radius.circular(60)
                     )
                 ),
-                child: InputWrapper(),
+                //InputWrapper
+                child: Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height: 40,),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10)
+                        ),
+                        //inputfield
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              padding: EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                  border: Border(
+                                      bottom: BorderSide(color: Colors.grey)
+                                  )
+                              ),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    hintText: "Enter Your Email",
+                                    hintStyle: TextStyle(color: Colors.grey),
+                                    border: InputBorder.none
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                  border: Border(
+                                      bottom: BorderSide(color: Colors.grey)
+                                  )
+                              ),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    hintText: "Enter Your Password",
+                                    hintStyle: TextStyle(color: Colors.grey),
+                                    border: InputBorder.none
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 40,),
+
+                      ElevatedButton(
+                        child: const Text('Login'),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => PageMain()),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          fixedSize: const Size(250, 50), primary: Colors.cyan,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
           ],
